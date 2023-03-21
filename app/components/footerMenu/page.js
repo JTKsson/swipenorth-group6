9f0f2c3dbda50546ed06d548161e453eb27c315e
Blixt-A <annica.blixt@edu.cmeducations.se>
2023-03-21 14:18:39 +0100

    add footer

---
+++ app/components/footerMenu/page.js
@@ -0,0 +1,38 @@
+import Link from 'next/link'
+import { faUser } from "@fortawesome/free-regular-svg-icons";
+import { faHeart } from "@fortawesome/free-regular-svg-icons";
+import { faCompass } from "@fortawesome/free-regular-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="container">
+        <nav>
+          <ul className="footer-menu">
+            <li className='menu-item'>
+              <Link href="/" className='menu-link'>
+                <FontAwesomeIcon icon={faCompass} className="menu-fa"/>
+                Utforska
+              </Link>
+            </li>
+            <li className='menu-item'>
+              <Link href="/" className='menu-link'>
+                <FontAwesomeIcon icon={faHeart} className="menu-fa"/>
+                Sparade jobb
+              </Link>
+            </li>
+            <li className='menu-item'>
+              <Link href="/" className='menu-link'>
+                <FontAwesomeIcon icon={faUser} className="menu-fa"/>
+                Profil
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
