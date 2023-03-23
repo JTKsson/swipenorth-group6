36d11623af5e5cd84339a3eae5c4d97256389ff5
ceridwenroberts <116894679+ceridwenroberts@users.noreply.github.com>
2023-03-23 14:42:46 +0100

    minor error handling

--- app/components/jobBox/page.js
+++ app/components/jobBox/page.js
@@ -1,7 +1,6 @@
 import React from "react";
 import jobBox from "./jobBox.css";
 
-
 export default function JobBox() {
   return (
     <>
@@ -16,17 +15,18 @@ export default function JobBox() {
           <span className="subSubSubHeading">Omfattning &nbsp;</span>
           <span className="scope">100%</span>
         </p>
-        <p>
+        
           <p className="about">Om tjänsten</p>
+          <p>
           <span className="jobDescription">
             Lorem Ipsum is simply dummy text of the printing and typesetting
             industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s &nbsp;
+            ever since the 1500s
             <span className="employerLink">Sök jobbet</span>
           </span>
         </p>
-        <h4 className="applyBy"><span>
-          sista ansökningsdatum </span> <br></br>
+        <h4 className="applyBy">
+          <span>sista ansökningsdatum </span>
           <span className="date">2023-05-27</span>
         </h4>
         <div className="btnContainer">
