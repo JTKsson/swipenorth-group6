21d6a8f4174e1729cd35e064b8c29e41ef54075d
ceridwenroberts <116894679+ceridwenroberts@users.noreply.github.com>
2023-03-23 10:52:31 +0100

    In app-layout , changed --const Layout = ...-- to --function RootLayout-- and added  Header and Footer there instead

---
+++ app/swipe/Drinks.js
@@ -0,0 +1,26 @@
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import styles from "./page.module.css";
+import bigZeroCola from "./array";
+import styles from "./Drinks.module.css";
+import {useSwipeable} from "react-swipeable";
+
+const inter = Inter({ subsets: ["latin"] });
+
+let drinks = bigZeroCola();
+
+export default function Drinks(props) {
+  const handlers = useSwipeable({
+    onSwiped: (eventData) => console.log("User Swiped!", eventData),
+    
+  });
+  
+  return (
+    <>
+      <h1>SwipeTest Page</h1>
+      <div className='card' key={id} {...handlers}/*onClick={handleClick}*/>
+      {drinks}
+      </div>
+    </>
+  );
+}
