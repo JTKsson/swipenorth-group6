ab8f29acb8f710295da85f8ff378605a7583cbfc
ceridwenroberts <116894679+ceridwenroberts@users.noreply.github.com>
2023-03-24 12:06:39 +0100

    created swipeTestPage, swipeCSS and test page with dummy swipe card

---
+++ app/swipeTestPage/page.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./swipeCSS.css"
+
+export default function page() {
+  return (
+    <div className='swipeContainer'>
+        <div className='card'>Card</div>
+    </div>
+  )
+}
