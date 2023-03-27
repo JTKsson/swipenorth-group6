3a74c71677cb4e1ce52dd5216f649c3ca6f59dcf
Excaliderp <timothy.karlsson@edu.cmeducations.se>
2023-03-27 16:36:56 +0200

    fixed tutorial position

--- app/components/tutorial/tutorial.js
+++ app/components/tutorial/tutorial.js
@@ -4,8 +4,10 @@ const Tutorial = () => {
   return (
     <div className={styles.tutorialCard}> 
       <p className={`${styles.arrow} ${styles.up}`}>Swipe upp</p>
+      <div className={styles.leftRight}>
       <p className={`${styles.arrow} ${styles.left}`}>Swipe vänster</p>
       <p className={`${styles.arrow} ${styles.right}`}>Swipe höger</p>
+      </div>
       <p className={`${styles.arrow} ${styles.down}`}>Swipe ner</p>
     </div>
   )
