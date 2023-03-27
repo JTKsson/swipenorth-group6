33ec69f4d8391bae1a6c355f9eb935d74b9e7d27
Blixt-A <annica.blixt@edu.cmeducations.se>
2023-03-27 14:25:43 +0200

    add temporary icon to footer

--- app/components/footerMenu/footerMenu.js
+++ app/components/footerMenu/footerMenu.js
@@ -1,10 +1,11 @@
 'use client'
 import { useRouter } from 'next/navigation';
 import Link from 'next/link'
-import { faUser } from "@fortawesome/free-regular-svg-icons";
+import { faFilter } from "@fortawesome/free-regular-svg-icons";
 import { faHeart } from "@fortawesome/free-regular-svg-icons";
 import { faCompass } from "@fortawesome/free-regular-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 
 const Footer = () => {
   const router = useRouter();
@@ -21,14 +22,14 @@ const Footer = () => {
             </li>
             <li className='menu-item'>
               <Link href="../savedJobs" className='menu-link'>
-                <FontAwesomeIcon icon={faHeart} className="menu-fa"/>
+                <FontAwesomeIcon icon={faHeart} className="menu-fa" />
                 Sparade jobb
               </Link>
             </li>
             <li className='menu-item'>
               <Link href="../profilePage" className='menu-link'>
-                <FontAwesomeIcon icon={faUser} className="menu-fa" />
-                Profil
+              <FontAwesomeIcon icon={faHeart} className="menu-fa" />
+                  Filter
               </Link>
             </li>
           </ul>
