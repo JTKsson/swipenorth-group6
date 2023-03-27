74f8814ecbd7cb14689eb8cc80dc3dca7287dc4f
Excaliderp <timothy.karlsson@edu.cmeducations.se>
2023-03-27 14:17:55 +0200

    started tutorial screen

---
+++ app/components/tutorial/tutorial.js
@@ -0,0 +1,14 @@
+import styles from "./tutorial.module.css"
+
+const Tutorial = () => {
+  return (
+    <div className={styles.tutorialCard}> 
+      <p className={`${styles.arrow} ${styles.up}`}>Swipe upp</p>
+      <p className={`${styles.arrow} ${styles.left}`}>Swipe vänster</p>
+      <p className={`${styles.arrow} ${styles.right}`}>Swipe höger</p>
+      <p className={`${styles.arrow} ${styles.down}`}>Swipe ner</p>
+    </div>
+  )
+}
+
+export default Tutorial
