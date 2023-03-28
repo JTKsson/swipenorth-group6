d4ed0aafcb8b8ba8ec619f00178e0fe6ab57deb6
Excaliderp <timothy.karlsson@edu.cmeducations.se>
2023-03-28 14:18:07 +0200

    first scrap of tutorial, needs detailing and colors

--- app/components/tutorial/tutorial.js
+++ app/components/tutorial/tutorial.js
@@ -1,15 +1,33 @@
+"use client"
+import { useState, useEffect } from "react";
 import styles from "./tutorial.module.css"
-import { TfiAngleDoubleUp, TfiAngleDoubleRight, TfiAngleDoubleLeft, TfiAngleDoubleDown} from "react-icons/tfi";
+import { TfiAngleDoubleUp, TfiAngleDoubleRight, TfiAngleDoubleLeft, TfiAngleDoubleDown, TfiHandPointUp} from "react-icons/tfi";
+
 const Tutorial = () => {
+
+  const [isHidden, setIsHidden] = useState(
+    localStorage.getItem("isHidden") === "true" // Check if isHidden is set to true in localStorage
+  );
+
+  const handleHide = () => {
+    setIsHidden(true);
+    localStorage.setItem("isHidden", "true"); // Store isHidden as true in localStorage
+  };
+
   return (
-    <div className={styles.tutorialCard}> 
-      <p className={`${styles.arrow} ${styles.up}`}><TfiAngleDoubleUp />Swipe upp</p>
+    <>
+    {isHidden ? null : <div className={styles.tutorialCard}> 
+      <p className={`${styles.arrow} ${styles.up}`}><TfiAngleDoubleUp />upp</p>
       <div className={styles.leftRight}>
-      <p className={`${styles.arrow} ${styles.left}`}>Swipe vänster<TfiAngleDoubleLeft /></p>
-      <p className={`${styles.arrow} ${styles.right}`}>Swipe höger<TfiAngleDoubleRight /></p>
+      <p className={`${styles.arrow} ${styles.left}`}>vänster<TfiAngleDoubleLeft /></p>
+      <p className={`${styles.arrow} ${styles.right}`}>höger<TfiAngleDoubleRight /></p>
       </div>
-      <p className={`${styles.arrow} ${styles.down}`}>Swipe ner<TfiAngleDoubleDown /></p>
+      <p className={`${styles.pointer} ${styles.arrow}`}><TfiHandPointUp /> Tryck och håll</p>
+      <p className={`${styles.arrow} ${styles.down}`}>ner<TfiAngleDoubleDown /></p>
+      <button className={styles.hide} onClick={handleHide}>Okej!</button>
     </div>
+    }
+    </>
   )
 }
 
