594119bcd1f21f51023e280be3fc1d0da2dda35b
Blixt-A <annica.blixt@edu.cmeducations.se>
2023-03-28 20:51:13 +0200

    add test updates to footer

--- app/components/footerMenu/footerMenu.js
+++ app/components/footerMenu/footerMenu.js
@@ -1,34 +1,37 @@
 'use client'
-import { useRouter } from 'next/navigation';
 import Link from 'next/link'
+import { useRouter } from 'next/navigation';
 import { faUser } from "@fortawesome/free-regular-svg-icons";
 import { faHeart } from "@fortawesome/free-regular-svg-icons";
 import { faCompass } from "@fortawesome/free-regular-svg-icons";
+import { faSort, faFilter } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Footer = () => {
   const router = useRouter();
   return (
     <footer className="footer">
-      
         <nav>
           <ul className="footer-menu">
             <li className='menu-item'>
               <Link href="/" className="menu-link">
+                <a className={router.pathname === '/' ? 'active' : ''} >hej</a>
                 <FontAwesomeIcon icon={faCompass} className="menu-fa" />
                 Utforska
               </Link>
             </li>
             <li className='menu-item'>
               <Link href="../savedJobs" className='menu-link'>
-                <FontAwesomeIcon icon={faHeart} className="menu-fa"/>
+              <a className={router.pathname === '/profilePage' ? 'active' : ''} >hej</a>
+                <FontAwesomeIcon icon={faHeart} className="menu-fa" / >
                 Sparade jobb
               </Link>
             </li>
             <li className='menu-item'>
               <Link href="../profilePage" className='menu-link'>
-                <FontAwesomeIcon icon={faUser} className="menu-fa" />
-                Profil
+              <a className={router.pathname === 'profilePage' ? 'active' : ''} />
+                <FontAwesomeIcon icon={faFilter} className="menu-fa" />
+                Filter
               </Link>
             </li>
           </ul>
