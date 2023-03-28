242e9aa077b1a8c0c32b7c83fe983ddd66bf4769
Excaliderp <timothy.karlsson@edu.cmeducations.se>
2023-03-28 22:40:20 +0200

    still figuring out bugs and vercel

--- app/components/tutorial/tutorial.js
+++ app/components/tutorial/tutorial.js
@@ -1,32 +1,41 @@
 "use client"
-import { React, useState } from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./tutorial.module.css"
 import { TfiAngleDoubleUp, TfiAngleDoubleRight, TfiAngleDoubleLeft, TfiAngleDoubleDown, TfiHandPointUp} from "react-icons/tfi";
 
 const Tutorial = () => {
 
-  const [isHidden, setIsHidden] = useState(
-    localStorage.getItem("isHidden") === "true" // Check if isHidden is set to true in localStorage
-  );
+  const [isHidden, setIsHidden] = useState(false);
+
+  useEffect(() => {
+    const storedIsHidden = localStorage.getItem("isHidden");
+    // localStorage.getItem("isHidden");
+    if (storedIsHidden === "false") {
+      setIsHidden(false);
+    }
+    //  else {
+    //   setIsHidden(true);
+    // }
+  }, []);
 
   const handleHide = () => {
     setIsHidden(true);
-    localStorage.setItem("isHidden", "true"); // Store isHidden as true in localStorage
+    localStorage.setItem("isHidden", "true");
   };
 
   return (
     <>
-    {isHidden ? null : <div className={styles.tutorialCard}> 
+    {!isHidden && ( <div className={styles.tutorialCard}> 
       <p className={`${styles.arrow} ${styles.up}`}><TfiAngleDoubleUp />upp</p>
       <div className={styles.leftRight}>
-      <p className={`${styles.arrow} ${styles.left}`}>vänster<TfiAngleDoubleLeft /></p>
-      <p className={`${styles.arrow} ${styles.right}`}>höger<TfiAngleDoubleRight /></p>
+       <p className={`${styles.arrow} ${styles.left}`}>vänster<TfiAngleDoubleLeft /></p>
+       <p className={`${styles.arrow} ${styles.right}`}>höger<TfiAngleDoubleRight /></p>
       </div>
       <p className={`${styles.pointer} ${styles.arrow}`}><TfiHandPointUp /> Tryck och håll</p>
       <p className={`${styles.arrow} ${styles.down}`}>ner<TfiAngleDoubleDown /></p>
       <button className={styles.hide} onClick={handleHide}>Okej!</button>
     </div>
-    }
+    )}
     </>
   )
 }
