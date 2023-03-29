bd2ba01551729b853a85ff63d6baf7e853883bee
Edu <edu.garma97@hotmail.com>
2023-03-29 13:23:02 +0200

    sparade Jobb

--- app/sparadeJobb/page.js
+++ app/sparadeJobb/page.js
@@ -1,10 +1,25 @@
 "use client";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import styles from "../sparadeJobb/page.module.css";
 
 const page = () => {
   const [showPopup, setShowPopup] = useState(false);
   const [popupMessage, setPopupMessage] = useState("");
+  const [boxes, setBoxes] = useState([]);
+
+  useEffect(() => {
+    // data from API
+    fetch("")
+      .then(response => response.json())
+      .then(data => {
+        //boxes with data from API
+        setBoxes(data.map(item => ({
+          title: item.title,
+          content: item.content
+        })));
+      })
+      .catch(error => console.error(error));
+  }, []);
 
   const handleTabClick = (message) => {
     setShowPopup(true);
@@ -13,69 +28,19 @@ const page = () => {
 
   return (
     <div className={styles.box.container}>
-    <div className={styles.box} onClick={() => handleTabClick("Content for städare")}>
-        <h2>städare</h2>
-        <p>t is a long established fact that a reader will
-           be distracted by the readable content of a 
-           page when looking at its layout. The point
-            of using Lorem Ipsum is that it has a
-             more-or-less normal distribution</p>
-      </div>
-      <div className={styles.box} onClick={() => handleTabClick("Content för taxi")}>
-        <h2>Taxi</h2>
-        <p>t is a long established fact that a reader will
-           be distracted by the readable content of a 
-           page when looking at its layout. The point
-            of using Lorem Ipsum is that it has a
-             more-or-less normal distribution</p>
-      </div>
-      <div className={styles.box} onClick={() => handleTabClick("Content för")}>
-        <h2>Title</h2>
-        <p>t is a long established fact that a reader will
-           be distracted by the readable content of a 
-           page when looking at its layout. The point
-            of using Lorem Ipsum is that it has a
-             more-or-less normal distribution</p>
-      </div>
-      <div className={styles.box} onClick={() => handleTabClick("Content för")}>
-        <h2>Title</h2>
-        <p>t is a long established fact that a reader will
-           be distracted by the readable content of a 
-           page when looking at its layout. The point
-            of using Lorem Ipsum is that it has a
-             more-or-less normal distribution</p>
-      </div>
-      <div className={styles.box} onClick={() => handleTabClick("Content for")}>
-        <h2>Title</h2>
-        <p>t is a long established fact that a reader will
-           be distracted by the readable content of a 
-           page when looking at its layout. The point
-            of using Lorem Ipsum is that it has a
-             more-or-less normal distribution</p>
-      </div>
-      <div className={styles.box} onClick={() => handleTabClick("Content för")}>
-        <h2>Title</h2>
-        <p>t is a long established fact that a reader will
-           be distracted by the readable content of a 
-           page when looking at its layout. The point
-            of using Lorem Ipsum is that it has a
-             more-or-less normal distribution</p>
-      </div>
-      <div className={styles.box} onClick={() => handleTabClick("Content för")}>
-        <h2>Title</h2>
-        <p>t is a long established fact that a reader will
-           be distracted by the readable content of a 
-           page when looking at its layout. The point
-            of using Lorem Ipsum is that it has a
-             more-or-less normal distribution</p>
-      </div>
+      {boxes.map((box, index) => (
+        <div className={styles.box} key={index} onClick={() => handleTabClick(box)}>
+          <h2>{box.title}</h2>
+          <p>{box.content}</p>
+        </div>
+      ))}
       {showPopup && (
-  <div className={styles.popup}>
-    <h2>{popupMessage.title}</h2>
-    <p>{popupMessage.paragraph}</p>
-    <button onClick={() => setShowPopup(false)}>Close</button>
-  </div>
-)}
+        <div className={styles.popup}>
+          <h2>{popupMessage.title}</h2>
+          <p>{popupMessage.content}</p>
+          <button onClick={() => setShowPopup(false)}>Close</button>
+        </div>
+      )}
     </div>
   );
 };
