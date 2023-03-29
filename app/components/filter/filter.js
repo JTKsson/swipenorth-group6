9a7388bed17b78c4689c3d1a3d88a774ce3a2d19
Excaliderp <timothy.karlsson@edu.cmeducations.se>
2023-03-29 17:00:25 +0200

    working on checkboxes and shit

--- app/components/filter/filter.js
+++ app/components/filter/filter.js
@@ -1,6 +1,68 @@
-const Filter  = () => {
+import { useState } from "react";
+// import annonserArr from "../../../pages/api/jobbannonser/jobbannonser.js"
 
-  return {
-    
-  }
+function FilteredList({ annonserArr }) {
+  const [selectedFilters, setSelectedFilters] = useState([]);
+
+  const handleCheckboxChange = (e) => {
+    const { value } = e.target;
+    if (selectedFilters.includes(value)) {
+      setSelectedFilters(selectedFilters.filter((filter) => filter !== value));
+    } else {
+      setSelectedFilters([...selectedFilters, value]);
+    }
+  };
+
+  const filteredList = annonserArr.filter((annonse) =>
+    selectedFilters.includes(annonse.category)
+  );
+
+  return (
+    <div>
+      <h2>Filter by category:</h2>
+      <ul>
+        <li>
+          <label>
+            <input
+              type="checkbox"
+              value="category1"
+              onChange={handleCheckboxChange}
+              checked={selectedFilters.includes("category1")}
+            />
+            Category 1
+          </label>
+        </li>
+        <li>
+          <label>
+            <input
+              type="checkbox"
+              value="category2"
+              onChange={handleCheckboxChange}
+              checked={selectedFilters.includes("category2")}
+            />
+            Category 2
+          </label>
+        </li>
+        <li>
+          <label>
+            <input
+              type="checkbox"
+              value="category3"
+              onChange={handleCheckboxChange}
+              checked={selectedFilters.includes("category3")}
+            />
+            Category 3
+          </label>
+        </li>
+      </ul>
+      <h2>Filtered list:</h2>
+      <ul>
+        {filteredList.map((annonse) => (
+          <li key={annonse.id}>{annonse.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
+
+export default FilteredList
--- app/profilePage/page.js
+++ app/profilePage/page.js
@@ -2,14 +2,17 @@
 
 import { Inter } from "next/font/google";
 import PlayingCards from "../components/playingCards/playingCards";
+import FilteredList from "../components/filter/filter";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function ProfilePage() {
   return (
-    <>
+    <>    
+    <FilteredList />
     <div className="pagePlaceholder">Profilsida
     </div>
+
     <PlayingCards />
     </>
   );
