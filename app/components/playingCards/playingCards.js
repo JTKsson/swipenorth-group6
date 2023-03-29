a1c302414c07d9cec40bfa90867e2c017e11bcf3
ceridwenroberts <116894679+ceridwenroberts@users.noreply.github.com>
2023-03-29 12:42:43 +0200

    ids added to jobbannonser.js

--- app/components/playingCards/playingCards.js
+++ app/components/playingCards/playingCards.js
@@ -8,6 +8,7 @@ import styles from "./styles.module.css";
 
 const annonserArr = [
   {
+    id: 1,
     workTitle: "Platschef",
     company: "Sodexo AB",
     workTime: "100%",
@@ -17,6 +18,7 @@ const annonserArr = [
       "Som platschef är du ansvarig för Sodexos uppdrag, som bland annat består av industri, kontorsstädning, receptions- och bokningstjänster. I rollen som platschef har du det yttersta ansvaret för leveransen mot kund där du agerar lyhört för kundernas behov samt säkerställer att Sodexo fullföljer sitt uppdrag på bästa sätt. Du kommer även ingå i Sodexos ledningsgrupp för Region Norr.",
   },
   {
+    id: 2,
     workTitle: "Servicetekniker",
     company: "2Complete AB",
     time: "100%",
@@ -26,6 +28,7 @@ const annonserArr = [
       "Arbetsuppgifterna består i huvudsak av service, förebyggande underhåll och reparation av industriportar och dockningslösningar. Arbetet bedrivs huvudsakligen på egen hand ute hos företagets kunder i Skellefteå med omnejd. Du är kundens kontaktperson och deras ansikte utåt i deras serviceorganisation. I denna tjänst förekommer också installationer av industriportar och dockningslösningar.",
   },
   {
+    id: 3,
     workTitle: "Produktionstekniker",
     company: "Minnovation International AB",
     time: "75%",
@@ -35,6 +38,7 @@ const annonserArr = [
       "Som produktionstekniker är du ansvarig för produktionsprocesserna på vår produktionsanläggning. Dina arbetsuppgifter kan exempelvis bestå av kvalitets- och miljöarbete, analysera och förbättra produktionsprocesserna, utveckla arbetsmetoder och även genomförande av dessa, utveckla strukturer för prissättning och ett nära samarbete med de olika avdelningarna för en så effektiv produktionsprocess som möjligt.",
   },
   {
+    id: 4,
     workTitle: "Sjuksköterska, Akuten",
     company: "Region Västerbotten, Akutmottagning Skellefteå",
     time: "100%",
@@ -44,6 +48,7 @@ const annonserArr = [
       "Som medarbetare på akuten möter du hela tiden nya patienter med olika typer av medicinska diagnoser och olycksfall. Det är ett stimulerande arbete som innebär nära samarbete mellan kollegor, yrkesgrupper och andra kliniker.",
   },
   {
+    id: 5,
     workTitle: "Industriarbetare",
     company: "Randstad",
     time: "50%",
@@ -167,6 +172,7 @@ function Deck() {
                 <p>{cards[i].lastApplyDate}</p>
                 <p>{cards[i].description}</p>
                 <button>Dela</button>
+                <p>lägga till seen/unseen?</p>
               </div>
             </animated.div>
           </animated.div>
