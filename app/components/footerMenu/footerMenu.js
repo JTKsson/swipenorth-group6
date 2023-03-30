9142c8b142d182c2af28b9e8725d566063859340
Blixt-A <annica.blixt@edu.cmeducations.se>
2023-03-30 12:00:15 +0200

    Finish active page function, will add styling later

--- app/components/footerMenu/footerMenu.js
+++ app/components/footerMenu/footerMenu.js
@@ -1,36 +1,53 @@
 'use client'
 import Link from 'next/link'
-import { useRouter } from 'next/navigation';
 import { faHeart } from "@fortawesome/free-regular-svg-icons";
 import { faCompass } from "@fortawesome/free-regular-svg-icons";
-import { faSliders} from "@fortawesome/free-solid-svg-icons";
+import { faFilter} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { usePathname } from 'next/navigation';
+import { VscCompass } from 'react-icons/fa';
+import { IconName } from "react-icons/ai";
+import { CiSliderHorizontal, CiHeart, CiCompass1 } from "react-icons/ci"
+
 
 const Footer = () => {
-  const router = useRouter();
+  const pathname = usePathname();
+
+  console.log(pathname);
   return (
     <footer className="footer">
         <nav>
           <ul className="footer-menu">
             <li className='menu-item'>
-              <Link href="/" className="menu-link">
-                <a className={router.pathname === '/' ? 'active' : ''} >hej</a>
-                <FontAwesomeIcon icon={faCompass} className="menu-fa" />
-                Utforska
+              <Link 
+                href="/" 
+                className={'menu-link' + (pathname === '/' ? ' active' : '')}
+              >
+                 <div className='menu-fa' >
+                <CiCompass1/>
+                </div>
               </Link>
             </li>
             <li className='menu-item'>
-              <Link href="../savedJobs" className='menu-link'>
-              <a className={router.pathname === 'savedJobs' ? 'active' : ''} >hej</a>
-                <FontAwesomeIcon icon={faHeart} className="menu-fa" / >
-                Sparade jobb
+            <Link 
+                href="../savedJobs" 
+                className={'menu-link' + (pathname === '/savedJobs' ? ' active' : '')}
+              >
+               <div className='menu-fa' >
+                <CiHeart/>
+                </div>
               </Link>
             </li>
             <li className='menu-item'>
-              <Link href="../profilePage" className='menu-link'>
-              <a className={router.pathname === 'profilePage' ? 'active' : ''} />
-                <FontAwesomeIcon icon={faSliders} className="menu-fa" />
-                Filter
+
+            <Link 
+                href="../profilePage" 
+                className={'menu-link' + (pathname === '/profilePage' ? ' active' : '')}
+              >
+                <div className='menu-fa' >
+                <CiSliderHorizontal/>
+                </div>
+              
               </Link>
             </li>
           </ul>
