a1ac1d4225d19154bedf223841333b66bc5c0311
Blixt-A <annica.blixt@edu.cmeducations.se>
2023-03-30 15:09:37 +0200

    addig temporary filter function

--- app/components/filterSortIconComp.js
+++ app/components/filterSortIconComp.js
@@ -1,9 +1,56 @@
-import React from 'react'
+// import React from 'react'
 
-function filterSortIconComp() {
+// function filterSortIconComp() {
+//   return (
+//     <div>filterSortIconComp</div>
+//   )
+// }
+
+// export default filterSortIconComp
+'use client'
+import { useState } from 'react'
+
+const CheckboxList = ({ options, onFilter }) => {
+  const [checkedOptions, setCheckedOptions] = useState([])
+
+  const handleCheckboxChange = (option) => {
+    const isChecked = checkedOptions.includes(option)
+
+    if (isChecked) {
+      setCheckedOptions(checkedOptions.filter((checkedOption) => checkedOption !== option))
+    } else {
+      setCheckedOptions([...checkedOptions, option])
+    }
+  }
+
+  const handleFilter = () => {
+    onFilter(checkedOptions)
+  }
+
   return (
-    <div>filterSortIconComp</div>
+    <>
+    <div>
+      {options.map((option, index) => (
+        <div key={index}>
+          <input
+            type="checkbox"
+            id={option}
+            name={option}
+            checked={checkedOptions.includes(option)}
+            onChange={() => handleCheckboxChange(option)}
+          />
+          <label htmlFor={option}>{option}</label>
+        </div>
+      ))}
+    </div>
+    </>
   )
 }
 
-export default filterSortIconComp
+export default CheckboxList
+
+
+
+
+
+
--- app/profilePage/page.js
+++ app/profilePage/page.js
@@ -1,16 +1,39 @@
 'use client'
 
-import { Inter } from "next/font/google";
-import PlayingCards from "../components/playingCards/playingCards";
+// import { Inter } from "next/font/google";
+// import PlayingCards from "../components/playingCards/playingCards";
+import CheckboxList from '../components/filterSortIconComp'
 
-const inter = Inter({ subsets: ["latin"] });
+const options = ['IT', 'Sjukvård', 'Skola', 'Transport']
+const salary = ['25 0000 - 30 000', '31 000 - 36 000']
+const remote = ['Ja', 'Nej', 'Delvis']
+const profession = ['Läkare','Lärare', 'Sjuksköterska', 'Utvecklare', 'Taxichafför']
 
-export default function ProfilePage() {
+const MyPage = () => {
+  const handleFilter = (checkedOptions) => {
+  }
+
   return (
     <>
-    <div className="pagePlaceholder">Profilsida
+    <div>
+      <h1>Branch</h1>
+      <CheckboxList options={options} onFilter={handleFilter} />
     </div>
-    <PlayingCards />
+    <div>
+      <h1>Yrke</h1>
+      <CheckboxList options={profession} onFilter={handleFilter} />
+    </div>
+    <div>
+      <h1>Lönspann</h1>
+      <CheckboxList options={salary} onFilter={handleFilter}/>
+    </div>
+    <div>
+      <h1>Möjlighet att jobba på distans</h1>
+      <CheckboxList options={remote} onFilter={handleFilter} />
+    </div>
+    <button onClick={handleFilter}>Filtrera</button>
     </>
-  );
+  )
 }
+
+export default MyPage
