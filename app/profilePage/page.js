c4dbaa57d4dc14c68eaf02f6d122ca809cc04e29
Blixt-A <annica.blixt@edu.cmeducations.se>
2023-03-31 11:30:47 +0200

    ...

--- app/profilePage/page.js
+++ app/profilePage/page.js
@@ -1,14 +1,38 @@
 'use client'
 
 import { Inter } from "next/font/google";
+import CheckboxList from '../components/filterSortIconComp'
 
-const inter = Inter({ subsets: ["latin"] });
+const options = ['IT', 'Sjukvård', 'Skola', 'Transport']
+const salary = ['25 0000 - 30 000', '31 000 - 36 000']
+const remote = ['Ja', 'Nej', 'Delvis']
+const profession = ['Läkare','Lärare', 'Sjuksköterska', 'Utvecklare', 'Taxichafför']
 
-export default function ProfilePage() {
+const MyPage = () => {
+  const handleFilter = (checkedOptions) => {
+  }
+
   return (
     <>
-    <div className="pagePlaceholder">Profilsida
+    <div>
+      <h1>Branch</h1>
+      <CheckboxList options={options} onFilter={handleFilter} />
     </div>
+    <div>
+      <h1>Yrke</h1>
+      <CheckboxList options={profession} onFilter={handleFilter} />
+    </div>
+    <div>
+      <h1>Lönspann</h1>
+      <CheckboxList options={salary} onFilter={handleFilter}/>
+    </div>
+    <div>
+      <h1>Möjlighet att jobba på distans</h1>
+      <CheckboxList options={remote} onFilter={handleFilter} />
+    </div>
+    <button onClick={handleFilter}>Filtrera</button>
     </>
-  );
+  )
 }
+
+export default MyPage
