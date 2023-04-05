7dd36730c397a7773ec7d1f5d3711add23627e7e
ceridwenroberts <116894679+ceridwenroberts@users.noreply.github.com>
2023-04-05 11:41:12 +0200

    remove user Window

--- app/page.js
+++ app/page.js
@@ -1,14 +1,13 @@
 import { Inter } from "next/font/google";
 import PlayingCards from "./components/playingCards/playingCards";
 import Tutorial from "./components/tutorial/tutorial";
-import UserWindow from "./components/UserWindow";
+
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <>
-    <UserWindow />
       <PlayingCards />
       <Tutorial />
     </>
