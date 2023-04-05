4be40a0a7b11d467b691170ec8d433848f1d252a
ceridwenroberts <116894679+ceridwenroberts@users.noreply.github.com>
2023-04-05 11:11:48 +0200

    user window test

--- app/page.js
+++ app/page.js
@@ -1,18 +1,17 @@
-'use client'
+"use client";
 import { Inter } from "next/font/google";
 import PlayingCards from "./components/playingCards/playingCards";
 import Tutorial from "./components/tutorial/tutorial";
-
-
-
+import UserWindow from "./components/UserWindow";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <>
-    <PlayingCards />
-    <Tutorial />
+      <UserWindow />
+      <PlayingCards />
+      <Tutorial />
     </>
   );
 }
